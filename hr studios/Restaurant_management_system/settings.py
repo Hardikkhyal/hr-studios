cef9b7b3f6c77b553d52c46acab0909f5f12e300
hardikkhyal <khyalhardik@gmail.com>
2026-08-05 22:32:00 +0530

    Remove python get-pip.py from requirements.txt

--- hr studios/Restaurant_management_system/settings.py
+++ hr studios/Restaurant_management_system/settings.py
@@ -52,12 +52,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'hr_studios.urls'
+ROOT_URLCONF = 'Restaurant_management_system.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['hr_studios/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'Restaurant_management_system', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'hr_studios.wsgi.application'
+WSGI_APPLICATION = 'Restaurant_management_system.wsgi.application'
 
 
 # Database
@@ -133,6 +133,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SMTP Configuration
 
